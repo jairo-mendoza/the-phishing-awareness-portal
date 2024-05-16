ba467c7b58b083254fedae0303386e48de2ec4be
jairo-mendoza <jairomendoza629@gmail.com>
2024-05-15 23:20:19 -0700

    Improve dashboard UI

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,6 +1,25 @@
 import { useUserStore } from '@/utils/userStore';
 import { useNavigate } from 'react-router-dom';
 
+import Stack from 'react-bootstrap/Stack';
+import { styled } from 'styled-components';
+
+const NavButton = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background-color: transparent;
+    border: 2px solid black;
+    border-radius: 10px;
+    padding: 10px;
+    font-size: 30px;
+    font-weight: bold;
+    min-width: 200px;
+    min-height: 75px;
+    text-align: center;
+`;
+
 const Dashboard = () => {
     const user = useUserStore((state) => state.user);
     const navigate = useNavigate();
@@ -12,8 +31,19 @@ const Dashboard = () => {
                 Welcome {user?.firstName} {user?.lastName}
             </p>
 
-            <button onClick={() => navigate('/training')}>Go to Training</button>
-            <button onClick={() => navigate('/forum')}>Go to Forum</button>
+            <Stack
+                direction="vertical"
+                gap={3}
+                style={{ alignItems: 'center', justifyContent: 'center', width: '100%' }}
+            >
+                <NavButton onClick={() => navigate('/training')}>
+                    <span style={{ fontSize: '40px' }}>⚔️</span> TRAIN
+                </NavButton>
+
+                <NavButton onClick={() => navigate('/forum')}>
+                    <span style={{ fontSize: '40px' }}>🗣️</span> FORUM
+                </NavButton>
+            </Stack>
         </div>
     );
 };
